d511dd8db9de1dfc4592de115d04d83e44b6cce9
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-07-14 08:43:50 +0600

    feat: add TaskBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import TaskBoard from "./components/task/TaskBoard";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <TaskBoard />
       <Footer />
     </>
   );
