f07a59ea85205cf09bc2aecb0930a11dbda9dabf
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-07-17 06:47:39 +0600

    feat: add new task modal and manage states

--- src/components/task/Taskboard.jsx
+++ src/components/task/Taskboard.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
+
+const defaultTasks = {
+  id: crypto.randomUUID(),
+  title: "Learn React",
+  description: "I want to learn react so that I can make scalable web applications",
+  tags: ["web", "react", "js"],
+  priority: "High",
+  isFavorite: true,
+};
 
 export default function TaskBoard() {
+  const [tasks, setTasks] = useState([defaultTasks]);
+  const [showModal, setShowModal] = useState(false);
+  
   return (
     <section className="mb-20" id="tasks">
+      {showModal && <AddTaskModal />}
       <div className="container mx-auto">
         {/* Search Box  */}
         <div className="p-2 flex justify-end">
@@ -12,8 +27,8 @@ export default function TaskBoard() {
         </div>
         {/* Search Box Ends */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
-          <TaskList />
+          <TaskActions addButtonClick={() => setShowModal(true)}/>
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
